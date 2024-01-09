92c75f5a1dd9dc3fe928406ccf53501b893c0d5c
CarlosDuqueDaex <caionaro0501@gmail.com>
2024-01-09 12:27:03 -0300

    Update hello.py

--- Jenkins/hello.py
+++ Jenkins/hello.py
@@ -7,7 +7,7 @@ venda = {'data': ['15/02/2021', '16/02/2021'],
 vendas_df = pd.DataFrame(venda) 
 print(vendas_df)
 
-cte_entrada = pd.read_csv('./cte_entrada.csv')
+cte_entrada = pd.read_csv('Jenkins/cte_entrada.csv')
 cte_entrada.head()
 
 print('Teste com sucesso')
