c52a83746ebfa8d5588a2bbaa1c8b9ff9c3d9e81
CarlosDuqueDaex <caionaro0501@gmail.com>
2024-01-10 10:33:40 -0300

    Update hello.py

--- Jenkins/hello.py
+++ Jenkins/hello.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from mysql.connector import connect
+
 venda = {'data': ['15/02/2021', '16/02/2021'],
          'valor': [500, 300],
          'produto': ['feijao', 'arroz'],
@@ -9,5 +11,24 @@ print(vendas_df)
 
 cte_entrada = pd.read_csv('Jenkins/cte_entrada.csv')
 print(cte_entrada.head())
+
+def mysql_connection(host, user, passwd, database=None):
+    connection = connect(
+        host = host,
+        user = user,
+        passwd = passwd,
+        database = database
+    )
+    return connection
+
+connection = mysql_connection('195.35.16.111', 'root', 'Duke1957')
+query = '''
+    SELECT * FROM dbProjetos.tbTeste
+'''
+cursor = connection.cursor()
+cursor.execute(query)
+result = cursor.fetchall()
+for row in result:
+    print(row)
 
 print('Teste com sucesso')
